f9c803351abef1a4507e173d68cf0775d6f96e16
Paco Chacon <pacochacon94@gmail.com>
2018-02-04 17:06:23 -0600

    Unified macOS and iOS in a single Sources file

---
+++ Sources/FCNetwork.h
@@ -0,0 +1,15 @@
+//
+//  FCNetwork.h
+//  FCNetwork
+//
+//  Created by Francisco Javier Chacon de Dios on 04/02/18.
+//
+
+@import Foundation
+
+FOUNDATION_EXPORT double FCNetworkVersionNumber;
+FOUNDATION_EXPORT const unsigned char FCNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FCNetwork/PublicHeader.h>
+
+
---
+++ Sources/FCNetwork.swift
@@ -0,0 +1,15 @@
+//
+//  FCNetwork.swift
+//  FCNetwork iOS
+//
+//  Created by Francisco Javier Chacon de Dios on 04/02/18.
+//
+
+import Foundation
+
+public class FCNetwork {
+
+    func get() {
+    }
+
+}
