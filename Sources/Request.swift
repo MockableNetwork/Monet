b62582033274a1cd9143e70fe4c3c1b63a10ecc5
Paco Chacon <pacochacon94@gmail.com>
2018-02-05 00:06:30 -0600

    Added HTTPMethod enum to Request

--- Sources/Request.swift
+++ Sources/Request.swift
@@ -1,8 +1,29 @@
 //
 //  Request.swift
-//  FCNetwork iOS
+//  FCNetwork
 //
 //  Created by Francisco Javier Chacon de Dios on 04/02/18.
 //
 
 import Foundation
+
+public enum HTTPMethod: String {
+    case get, put, patch, post, delete
+
+    func getMethodString() -> String {
+        switch self {
+        case .get:
+            return "GET"
+        case .put:
+            return "PUT"
+        case .patch:
+            return "PATCH"
+        case .post:
+            return "POST"
+        case .delete:
+            return "DELETE"
+        default:
+            "GET"
+        }
+    }
+}
