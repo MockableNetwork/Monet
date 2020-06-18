df7803e44d8d3921bfa13d19b09442b12b09de4c
Francisco Chacon <pacochacon94@gmail.com>
2020-06-18 11:15:11 -0500

    Generate URLConvertible

---
+++ Sources/URLConvertible.swift
@@ -0,0 +1,13 @@
+//
+//  URLConvertible.swift
+//  Monet iOS
+//
+//  Created by Francisco Javier Chacon de Dios on 17/06/20.
+//  Copyright © 2020 Monet. All rights reserved.
+//
+
+import Foundation
+
+public protocol URLConvertible {
+    func toUrl() throws -> URL
+}
