e7230b05c850eb2a3604247cdfc721f7d03971dd
Francisco Javier Chacon de Dios <pacochacon94@gmail.com>
2020-06-18 11:25:33 -0500

    Add String+URLConvertible

---
+++ Sources/String+URLConvertible.swift
@@ -0,0 +1,19 @@
+//
+//  String+URLConvertible.swift
+//  Monet iOS
+//
+//  Created by Francisco Javier Chacon de Dios on 18/06/20.
+//  Copyright © 2020 Monet. All rights reserved.
+//
+
+import Foundation
+
+extension String: URLConvertible {
+    public func toUrl() throws -> URL {
+        guard let url = URL(string: self) else {
+            throw MNTError.invalidUrl(url: self)
+        }
+
+        return url
+    }
+}
