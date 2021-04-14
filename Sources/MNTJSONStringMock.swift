b0b7a14c00859c3a0b32dc8c8dc871f22735e9ac
Francisco Chacon <pacochacon94@gmail.com>
2021-04-14 14:21:33 -0500

    Add base file for JSONStringMock

---
+++ Sources/MNTJSONStringMock.swift
@@ -0,0 +1,9 @@
+//
+//  MNTJSONStringMock.swift
+//  Monet
+//
+//  Created by Francisco Javier Chacon de Dios on 14/04/21.
+//  Copyright © 2021 Monet. All rights reserved.
+//
+
+import Foundation
