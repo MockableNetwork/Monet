d82bdf67ccc9de2aa72d77977ecf83ba794e4953
Francisco Chacon <pacochacon94@gmail.com>
2021-04-16 12:35:26 -0500

    generate protocols for mockable url session and data task

---
+++ Sources/MNTMockableURLSession.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  Monet
+//
+//  Created by Francisco Javier Chacon de Dios on 15/04/21.
+//  Copyright © 2021 Monet. All rights reserved.
+//
+
+import Foundation
+
+typealias DataTaskResult = (Data?, URLResponse?, Error?) -> Void
+
+protocol MNTMockableURLSession {
+    func dataTask(request: URLRequest,
+                  _ completionHandler: @escaping DataTaskResult) -> MNTMockableDataTask
+}
