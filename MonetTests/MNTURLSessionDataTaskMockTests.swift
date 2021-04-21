274b1c2b2970e9a339abcd6100cdc02189410d10
Francisco Chacon <pacochacon94@gmail.com>
2021-04-21 15:41:50 -0500

    Add completion handler to MNTURLSessionDataTaskMock

--- MonetTests/MNTURLSessionDataTaskMockTests.swift
+++ MonetTests/MNTURLSessionDataTaskMockTests.swift
@@ -10,12 +10,107 @@ import XCTest
 @testable import Monet
 
 class MNTURLSessionDataTaskMockTests: XCTestCase {
-    func testResume() {
-        let dataTask = MNTURLSessionDataTaskMock()
+    func testResumeWithNoMockError() {
+        let dataTaskExpectation = expectation(description: "Data Task Response")
+        let dataTask = MNTURLSessionDataTaskMock(mock: nil) { (_, _, error) in
+            XCTAssertEqual(error?.localizedDescription,
+                           MNTError.mockFail(reason: .noMock).localizedDescription)
+            dataTaskExpectation.fulfill()
+        }
 
         XCTAssertFalse(dataTask.called)
 
         dataTask.resume()
         XCTAssertTrue(dataTask.called)
+
+        wait(for: [dataTaskExpectation], timeout: 2)
+    }
+
+    func testResumeWithWrongURLError() {
+        let dataTaskExpectation = expectation(description: "Data Task Response")
+        let mock = MNTMock(body: MNTJSONStringMock("{\"project\": \"Monet\"}"),
+                           error: nil,
+                           urlResponse: .init(url: "www.bad url.com", status: .success))
+        let dataTask = MNTURLSessionDataTaskMock(mock: mock) { (_, _, error) in
+            XCTAssertEqual(error?.localizedDescription,
+                           MNTError.invalidUrl(url: "www.bad url.com").localizedDescription)
+            dataTaskExpectation.fulfill()
+        }
+
+        XCTAssertFalse(dataTask.called)
+
+        dataTask.resume()
+        XCTAssertTrue(dataTask.called)
+
+        wait(for: [dataTaskExpectation], timeout: 2)
+    }
+
+    func testResumeWithWrongMockNotPresentInBundle() {
+        let dataTaskExpectation = expectation(description: "Data Task Response")
+        let mock = MNTMock(body: MNTFileMock(filename: "File",
+                                             fileExtension: "json",
+                                             bundle: Bundle(for: type(of: self))),
+                           error: nil,
+                           urlResponse: .init(url: "www.test.com", status: .success))
+        let dataTask = MNTURLSessionDataTaskMock(mock: mock) { (_, _, error) in
+            XCTAssertEqual(error?.localizedDescription,
+                           MNTError.mockFail(reason: .notPresentInBundle(file: "File.json")).localizedDescription)
+            dataTaskExpectation.fulfill()
+        }
+
+        XCTAssertFalse(dataTask.called)
+
+        dataTask.resume()
+        XCTAssertTrue(dataTask.called)
+
+        wait(for: [dataTaskExpectation], timeout: 2)
+    }
+
+    func testResumeWithSuccessFileMock() {
+        let dataTaskExpectation = expectation(description: "Data Task Response")
+        let mock = MNTMock(body: MNTFileMock(filename: "Mock",
+                                             fileExtension: "json",
+                                             bundle: Bundle(for: type(of: self))),
+                           error: nil,
+                           urlResponse: .init(url: "www.test.com", status: .success))
+        let dataTask = MNTURLSessionDataTaskMock(mock: mock) { (data, response, error) in
+            XCTAssertNotNil(data)
+            XCTAssertEqual(String(data: data ?? Data(), encoding: .utf8),
+                           "{\n    \"project\": \"Monet\"\n}\n")
+            XCTAssertNotNil(response)
+            XCTAssertEqual(response?.url, try? "www.test.com".toUrl())
+            XCTAssertNil(error)
+            dataTaskExpectation.fulfill()
+        }
+
+        XCTAssertFalse(dataTask.called)
+
+        dataTask.resume()
+        XCTAssertTrue(dataTask.called)
+
+        wait(for: [dataTaskExpectation], timeout: 2)
+    }
+
+    func testResumeWithSuccessJSONStringMock() {
+        let dataTaskExpectation = expectation(description: "Data Task Response")
+        let mock = MNTMock(body: MNTJSONStringMock("{\"project\": \"Monet\"}"),
+                           error: nil,
+                           urlResponse: .init(url: "www.test.com", status: .success))
+        let dataTask = MNTURLSessionDataTaskMock(mock: mock) { (data, response, error) in
+            XCTAssertNotNil(data)
+            XCTAssertEqual(String(data: data ?? Data(), encoding: .utf8),
+                           "{\"project\": \"Monet\"}")
+            XCTAssertNotNil(response)
+            XCTAssertEqual(response?.url, try? "www.test.com".toUrl())
+            XCTAssertNil(error)
+            dataTaskExpectation.fulfill()
+        }
+
+        XCTAssertFalse(dataTask.called)
+
+        dataTask.resume()
+        XCTAssertTrue(dataTask.called)
+
+        wait(for: [dataTaskExpectation], timeout: 2)
     }
 }
--- Sources/MNTURLSessionDataTaskMock.swift
+++ Sources/MNTURLSessionDataTaskMock.swift
@@ -10,8 +10,28 @@ import Foundation
 
 class MNTURLSessionDataTaskMock: MNTMockableDataTask {
     var called: Bool = false
+    var mock: MNTMock?
+    var completionHandler: DataTaskResult
+
+    init(mock: MNTMock?, completionHandler: @escaping DataTaskResult) {
+        self.mock = mock
+        self.completionHandler = completionHandler
+    }
 
     func resume() {
         called = true
+        guard let mock = mock else {
+            return completionHandler(nil,
+                                     nil,
+                                     MNTError.mockFail(reason: .noMock))
+        }
+
+        do {
+            let data = try mock.body.toData()
+            let response = try mock.urlResponse.httpResponse()
+            completionHandler(data, response, mock.error)
+        } catch {
+            completionHandler(nil, nil, error)
+        }
     }
 }
