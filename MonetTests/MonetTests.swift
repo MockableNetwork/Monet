9a7bd1b804b29f59243d4f7b85008053a42f0ca3
Francisco Chacon <pacochacon94@gmail.com>
2021-04-23 13:23:21 -0500

    Update work of Monet main module to have present request, setMock and clearMocks also, it can manage to get initialized with a configuration or use shared instance

--- MonetTests/MonetTests.swift
+++ MonetTests/MonetTests.swift
@@ -9,8 +9,104 @@ import XCTest
 import Monet
 
 class MonetTests: XCTestCase {
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override func setUpWithError() throws {
+        Monet.shared.clearMocks()
+    }
+
+    func testSharedInstanceURLSession() {
+        let monet: Monet = .shared
+
+        XCTAssert(monet.urlSession is MNTURLSessionMock)
+
+        let mock = MNTMock(body: "{\"project\": \"Monet\"}",
+                           error: nil,
+                           status: .success,
+                           urlConvertible: "www.test.com",
+                           method: .get)
+        let urlSession = monet.urlSession as! MNTURLSessionMock
+
+        monet.setMock(mock)
+
+        XCTAssertEqual(urlSession.mocks.count, 1)
+        XCTAssertEqual(mock, urlSession.mocks.first!)
+    }
+
+    func testInit() {
+        let configuration = MNTConfiguration(urlSession: MNTURLSession(),
+                                             urlSessionMock: MNTURLSessionMock(),
+                                             debugKey: "OTHER")
+        let monet = Monet(configuration: configuration)
+
+        XCTAssert(monet.urlSession is MNTURLSession)
+    }
+
+    func testRequestWithMocks() {
+        let successExpectation = expectation(description: "Success")
+        let monet: Monet = .shared
+        let bodyMock = MNTFileMock(filename: "Mock",
+                                   fileExtension: "json",
+                                   bundle: .init(for: type(of: self)))
+        let mock = MNTMock(body: bodyMock,
+                           error: nil,
+                           status: .success,
+                           urlConvertible: "www.test.com",
+                           method: .get)
+        monet.setMock(mock)
+
+        monet.request(fromUrl: "www.test.com",
+                      method: .get,
+                      headers: ["name": "Monet"]) { (data, response, error) in
+            XCTAssertNotNil(data)
+            XCTAssertNotNil(response)
+            XCTAssertNil(error)
+            successExpectation.fulfill()
+        }
+
+        wait(for: [successExpectation], timeout: 5)
+    }
+
+    func testRequestWithBadUrl() {
+        let successExpectation = expectation(description: "Failure")
+        let monet: Monet = .shared
+        let bodyMock = MNTFileMock(filename: "Mock",
+                                   fileExtension: "json",
+                                   bundle: .init(for: type(of: self)))
+        let mock = MNTMock(body: bodyMock,
+                           error: nil,
+                           status: .success,
+                           urlConvertible: "www.bad url.com",
+                           method: .get)
+        monet.setMock(mock)
+
+        monet.request(fromUrl: "www.bad url.com",
+                      method: .get,
+                      headers: ["name": "Monet"]) { (data, response, error) in
+            XCTAssertNil(data)
+            XCTAssertNil(response)
+            XCTAssertNotNil(error)
+            XCTAssertEqual(error?.localizedDescription,
+                           MNTError.invalidUrl(url: "www.bad url.com").localizedDescription)
+            successExpectation.fulfill()
+        }
+
+        wait(for: [successExpectation], timeout: 5)
+    }
+
+    func testRequestWithNoMock() {
+        let successExpectation = expectation(description: "Failure")
+        let monet: Monet = .shared
+
+        monet.request(fromUrl: "www.test.com",
+                      method: .get,
+                      headers: ["name": "Monet"]) { (data, response, error) in
+            XCTAssertNil(data)
+            XCTAssertNil(response)
+            XCTAssertNotNil(error)
+            XCTAssertEqual(error?.localizedDescription,
+                           MNTError.mockFail(reason: .noMock).localizedDescription)
+            successExpectation.fulfill()
+        }
+
+        wait(for: [successExpectation], timeout: 5)
     }
 }
--- Sources/Monet.swift
+++ Sources/Monet.swift
@@ -11,26 +11,34 @@ public typealias Headers = [String: String]
 public typealias Parameters = [String: Any]
 
 public class Monet {
-    public static let shared: Monet = { return Monet(urlSession: .shared) }()
-    var urlSession: URLSession
-
-    init(urlSession: URLSession) {
-        self.urlSession = urlSession
+    public static let shared: Monet = {
+        return Monet(configuration: .default)
+    }()
+    private var configuration: MNTConfiguration
+    public var urlSession: MNTMockableURLSession {
+        if ProcessInfo().arguments.contains(configuration.debugKey) {
+            return configuration.urlSessionMock
+        }
+        return configuration.urlSession
     }
 
-    func request(from urlString: URLConvertible,
-                 using method: HTTPMethod = .get,
-                 sending headers: Headers? = nil,
-                 and parameters: Parameters? = nil,
-                 setting timeout: TimeInterval = 30,
-                 cachePolicy: URLRequest.CachePolicy = .reloadIgnoringCacheData,
+    public init(configuration: MNTConfiguration) {
+        self.configuration = configuration
+    }
+
+    public func setMock(_ mock: MNTMock) { urlSession.setMock(mock) }
+
+    public func clearMocks() { urlSession.clearMocks() }
+
+    public func request(fromUrl urlConvertible: URLConvertible,
+                 method: HTTPMethod = .get,
+                 headers: Headers? = nil,
                  handler: @escaping(Data?, URLResponse?, Error?) -> Void) {
 
         do {
-            let url = try urlString.toUrl()
-            var urlRequest: URLRequest = URLRequest(url: url,
-                                                    cachePolicy: cachePolicy,
-                                                    timeoutInterval: timeout)
+            let url = try urlConvertible.toUrl()
+            var urlRequest: URLRequest = URLRequest(url: url)
+            urlRequest.httpMethod = method.getValue()
 
             if let headers = headers {
                 for (key, value) in headers {
@@ -38,13 +46,11 @@ public class Monet {
                 }
             }
 
-            URLSession.shared.dataTask(with: urlRequest,
-                                       completionHandler: { (data, response, error) in
-                                        handler(data, response, error)
-                                       }).resume()
+            urlSession.dataTask(request: urlRequest, { (data, response, error) in
+                handler(data, response, error)
+            }).resume()
         } catch {
             handler(nil, nil, error)
         }
     }
-
 }
